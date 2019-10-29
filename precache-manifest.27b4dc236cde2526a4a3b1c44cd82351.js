ce12fb08a77e3830262ca27a3bb3790aaa7cb659
unl148 <ksumana02@hotmail.com>
2019-10-28 19:38:28 -0700

    Updates

---
+++ precache-manifest.27b4dc236cde2526a4a3b1c44cd82351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a962ca4b867b9e150eb94515617dfcc",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "caab74aa8791981a4b35",
+    "url": "/Clicky-Game/static/css/main.cc7ba978.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/Clicky-Game/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "caab74aa8791981a4b35",
+    "url": "/Clicky-Game/static/js/main.c2631878.chunk.js"
+  },
+  {
+    "revision": "515a30886e7d4bdd716c",
+    "url": "/Clicky-Game/static/js/runtime-main.49db83b4.js"
+  }
+]);
